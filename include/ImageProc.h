bc24e00105c00f14c62a43014e6cf22f2c32eb70
Rishabh Battulwar <rishabhwork@gmail.com>
2014-09-13 13:10:43 -0700

    Oil Painting effect routine working

--- include/ImageProc.h
+++ include/ImageProc.h
@@ -12,13 +12,14 @@ class ImageProc
 	public:
     	static void color_to_bw(const char *infile, const char *outfile, int width, int height);
 		static void image_resize(const char *infile, const char* outfile, int width, int height, int target_width, int target_height);
+		static void hist_equal_cumulative(const char *infile, const char *outfile, int width, int height);
+		static void hist_equal(const char *infile, const char *outfile, int width, int height);
+		static void oil_painting(const char *infile, const char *outfile, int width, int height, int window_size);
 		
 };
 
 void ImageProc::color_to_bw(const char *infile, const char *outfile, int width, int height)
 {
-	//Image* img_color = new Image("color");
-	//Image* img_bw = new Image("bw", width, height);
 	Image img_color("color", width, height);
 	Image img_bw("bw", width, height);
 
@@ -70,6 +71,234 @@ void ImageProc::image_resize(const char *infile, const char* outfile, int width,
 
 	//img_resized.write_image(outfile, target_width, target_height);
 	img_resized.write_image(outfile, target_width, target_height);
+}
+
+void ImageProc::hist_equal_cumulative(const char *infile, const char *outfile, int width, int height)
+{
+	//########### If image == bw #########
+	// Image img_bw("bw", width, height);
+
+	// img_bw.read_image(infile, width, height);
+
+	// int histogram_bins[256] = {0};
+
+	// for(int i = 0; i < height; i++)
+	// 	for(int j = 0; j < width; j++)
+	// 		if((img_bw(i, j, 0) >= 0) && (img_bw(i, j, 0) < 256))
+	// 			histogram_bins[img_bw(i, j, 0)]++;
+
+
+	// int hist_cumulative[256] = {0};
+	// hist_cumulative[0] = histogram_bins[0];
+	// for(int i = 1; i < 256; i++)
+	// 	hist_cumulative[i] = hist_cumulative[i-1] + histogram_bins[i];
+
+
+	// for(int i = 0; i < height; i++)
+	// 	for(int j = 0; j < width; j++)
+	// 		if      (img_bw(i, j, 0) < 0  ) img_bw.imgdata[(i)*width*1 + (j)*1 + 0] = 0  ;
+	// 		else if (img_bw(i, j, 0) > 255) img_bw.imgdata[(i)*width*1 + (j)*1 + 0] = 255;
+	// 		else    img_bw.imgdata[(i)*width*1 + (j)*1 + 0] = 255 * hist_cumulative[img_bw(i,j,0)] \
+	// 														      / hist_cumulative[255];
+
+
+	// img_bw.write_image(outfile, width, height);
+
+	//######## If image == color ########
+
+	Image img_color("color", width, height);
+
+	img_color.read_image(infile, width, height);
+
+	int histogram_bins[256][3] = {0};
+
+	for(int i = 0; i < height; i++)
+		for(int j = 0; j < width; j++)
+			for(int k = 0; k < 3; k++)
+				if((img_color(i, j, k) >= 0) && (img_color(i, j, k) < 256))
+					histogram_bins[img_color(i, j, k)][k]++;
+
+
+	int hist_cumulative[256][3] = {0};
+	for(int k = 0; k < 3; k++)
+		hist_cumulative[0][k] = histogram_bins[0][k];
+	for(int i = 1; i < 256; i++)
+		for(int k = 0; k < 3; k++)
+			hist_cumulative[i][k] = hist_cumulative[i-1][k] + histogram_bins[i][k];
+
+
+	for(int i = 0; i < height; i++)
+		for(int j = 0; j < width; j++)
+			for(int k = 0; k < 3; k++)
+				if      (img_color(i, j, k) < 0  ) img_color.imgdata[(i)*width*3 + (j)*3 + k] = 0  ;
+				else if (img_color(i, j, k) > 255) img_color.imgdata[(i)*width*3 + (j)*3 + k] = 255;
+				else    img_color.imgdata[(i)*width*3 + (j)*3 + k] = 255 * hist_cumulative[img_color(i,j,k)][k] \
+																      / hist_cumulative[255][k];
+
+
+	img_color.write_image(outfile, width, height);
+
+	//###########
+
+}
+
+void ImageProc::hist_equal(const char *infile, const char *outfile, int width, int height)
+{
+	//########### If image == bw #########
+	// Image img_bw("bw", width, height);
+
+	// img_bw.read_image(infile, width, height);
+
+	// int histogram_bins[256] = {0};
+
+	// for(int i = 0; i < height; i++)
+	// 	for(int j = 0; j < width; j++)
+	// 		if((img_bw(i, j, 0) >= 0) && (img_bw(i, j, 0) < 256))
+	// 			histogram_bins[img_bw(i, j, 0)]++;
+
+
+	// int hist_cumulative[256] = {0};
+	// hist_cumulative[0] = histogram_bins[0];
+	// for(int i = 1; i < 256; i++)
+	// 	hist_cumulative[i] = hist_cumulative[i-1] + histogram_bins[i];
+
+	// int hist_cumulative_copy[256] = {0};
+	// int curr_bin_count = 0, prev_bin_count = 0;
+	// double value =  0;
+	// for(int i = 0; i < height; i++)
+	// 	for(int j = 0; j < width; j++)
+	// 		if      (img_bw(i, j, 0) < 0  ) img_bw.imgdata[(i)*width*1 + (j)*1 + 0] = 0  ;
+	// 		else if (img_bw(i, j, 0) > 255) img_bw.imgdata[(i)*width*1 + (j)*1 + 0] = 255;
+	// 		else    
+	// 		{
+	// 			++hist_cumulative_copy[img_bw(i, j, 0)];
+	// 			curr_bin_count = hist_cumulative_copy[img_bw(i, j, 0)];
+	// 			prev_bin_count = hist_cumulative[img_bw(i, j, 0) - 1];
+	// 			value = floor(255.0 * (prev_bin_count + curr_bin_count) / hist_cumulative[255]);
+	// 			img_bw.imgdata[(i)*width*1 + (j)*1 + 0] = value;
+	// 			value = 0; curr_bin_count = 0; prev_bin_count = 0;
+	// 		}
+
+	// img_bw.write_image(outfile, width, height);
+
+	//######## If image == color ########
+
+	Image img_color("color", width, height);
+
+	img_color.read_image(infile, width, height);
+
+	int histogram_bins[256][3] = {0};
+
+	for(int i = 0; i < height; i++)
+		for(int j = 0; j < width; j++)
+			for(int k = 0; k < 3; k++)
+				if((img_color(i, j, k) >= 0) && (img_color(i, j, k) < 256))
+					histogram_bins[img_color(i, j, k)][k]++;
+
+
+	int hist_cumulative[256][3] = {0};
+	for(int k = 0; k < 3; k++)
+		hist_cumulative[0][k] = histogram_bins[0][k];
+	for(int i = 1; i < 256; i++)
+		for(int k = 0; k < 3; k++)
+			hist_cumulative[i][k] = hist_cumulative[i-1][k] + histogram_bins[i][k];
+
+	int hist_cumulative_copy[256][3] = {0};
+	int curr_bin_count = 0, prev_bin_count = 0;
+	double value =  0;
+
+	for(int i = 0; i < height; i++)
+		for(int j = 0; j < width; j++)
+			for(int k = 0; k < 3; k++)
+				if      (img_color(i, j, k) < 0  ) img_color.imgdata[(i)*width*3 + (j)*3 + k] = 0  ;
+				else if (img_color(i, j, k) > 255) img_color.imgdata[(i)*width*3 + (j)*3 + k] = 255;
+				else    
+				{
+					++hist_cumulative_copy[img_color(i, j, 0)][k];
+					curr_bin_count = hist_cumulative_copy[img_color(i, j, 0)][k];
+					prev_bin_count = hist_cumulative[img_color(i, j, 0) - 1][k];
+					value = floor(255.0 * (prev_bin_count + curr_bin_count) / hist_cumulative[255][k]);
+					img_color.imgdata[(i)*width*3 + (j)*3 + k] = value;
+					value = 0; curr_bin_count = 0; prev_bin_count = 0;
+				}
+
+
+	img_color.write_image(outfile, width, height);
+
+	//###########
+
+}
+
+
+void ImageProc::oil_painting(const char *infile, const char *outfile, int width, int height, int window_size)
+{
+	Image img_orig("color", width, height);
+	Image img_oil("color", width, height);
+
+	int ws = window_size; //window_size
+	int wl = ((-1*ws) + 1) / 2;
+	int wr = (( 1*ws) - 1) / 2;
+	int values[ws*ws][3];
+	int max_occur_val[3], max_occurrence = 0, occurrence = 0;;
+	img_orig.read_image(infile, width, height);
+	int index;
+
+	for(int i = 0; i < height; i++)
+	 	for(int j = 0; j < width; j++)
+	 	{
+			index = 0;
+			max_occur_val[0] = max_occur_val[1] = max_occur_val[2] = 0;
+			max_occurrence = 0;
+			for(int p = 0; p < ws*ws; p++)
+				values[p][0] = values[p][1] = values[p][2] = 0;
+
+			for(int m = wl; m <= wr; m++)
+				for(int n = wl; n <= wr; n++)
+				{	
+					if((i+m >= 0) && (i+m < height) && (j+n >= 0) && (j+n < width))
+						for(int k = 0; k < 3; k++)
+						{
+							values[index][k] = img_orig(i+m, j+n, k);
+						}
+					else
+						for(int k = 0; k < 3; k++)
+							values[index][k] = 280;
+
+					index++;
+
+				}
+
+			for(int p = 0; p < ws*ws; p++)
+			{	
+				if(!(values[p][0] == 280) && !(values[p][1] == 280) && !(values[p][0] == 280))
+				{
+					occurrence = 0;
+					for(int r = 0; r < ws*ws; r++)
+					{
+						if(values[p][0] == values[r][0] &&
+						   values[p][1] == values[r][1] &&
+						   values[p][2] == values[r][2]) occurrence++;
+
+					}
+
+					if(max_occurrence < occurrence) 
+					{
+						max_occurrence = occurrence;
+						max_occur_val[0] = values[p][0];
+						max_occur_val[1] = values[p][1];
+						max_occur_val[2] = values[p][2];
+					}
+				}
+			}
+
+			img_oil.imgdata[i*width*3 + j*3 + 0] = max_occur_val[0];
+			img_oil.imgdata[i*width*3 + j*3 + 1] = max_occur_val[1];
+			img_oil.imgdata[i*width*3 + j*3 + 2] = max_occur_val[2];
+			
+			
+	 	}
+
+	img_oil.write_image(outfile, width, height);
 }
 
 
--- src/Main.cpp
+++ src/Main.cpp
@@ -6,7 +6,7 @@
 
 void usage(const char *exename)
 {
-	std::cout << "\n Usage: " << exename << " infile outfile [type] [-w width] [-h height] [-tw target_width] [-th target_height]" << std::endl;
+	std::cout << "\n Usage: " << exename << " infile outfile [type] [-w width] [-h height] [-tw target_width] [-th target_height] [-ws window_size]" << std::endl;
 	std::cout << " [options]:" << std::endl;
 	std::cout << "\t type :'bw'||'color' ; default: 'color'" << std::endl;
 	std::cout << "\t -w width : (default) 512" 				<< std::endl;
@@ -19,7 +19,8 @@ void usage(const char *exename)
 int main(int argc, char* argv[])
 {
 	int width = 512, height = 512;
-	int target_width = 700, target_height = 700;
+	int window_size = 5;
+	//int target_width = 700, target_height = 700;
 	char *type = "color";
 	if (argc < 3)
 		usage(argv[0]);
@@ -29,6 +30,7 @@ int main(int argc, char* argv[])
 		if(!(strcmp(argv[i], "bw")) || !(strcmp(argv[i], "color"))) type = argv[i];
 		else if(!(strcmp(argv[i], "-w"))) width = atoi(argv[++i]);
 		else if(!(strcmp(argv[i], "-h"))) height = atoi(argv[++i]);
+		else if(!(strcmp(argv[i], "-ws"))) window_size = atoi(argv[++i]);
 		else 
 		{
 			std::cerr << "\n Unknown Option : " << argv[i] << std::endl;
@@ -37,7 +39,10 @@ int main(int argc, char* argv[])
 	}
 
 	//ImageProc::color_to_bw(argv[1], argv[2], width, height);
-	ImageProc::image_resize(argv[1], argv[2], width, height, target_width, target_height);
+	//ImageProc::image_resize(argv[1], argv[2], width, height, target_width, target_height);
+	//ImageProc::hist_equal_cumulative(argv[1], argv[2], width, height);
+	//ImageProc::hist_equal(argv[1], argv[2], width, height);
+	ImageProc::oil_painting(argv[1], argv[2], width, height, window_size);
 	
 	return 0;
 }
